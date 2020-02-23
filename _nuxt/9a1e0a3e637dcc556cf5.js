bd52dee41151ecf98b3ff7febf9e199a6af0e075
Dror Tirosh <dror@openeth.dev>
2020-02-23 14:49:31 +0200

    added dynamic files

---
+++ _nuxt/9a1e0a3e637dcc556cf5.js
@@ -0,0 +1,104 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [4], {
+        1053: function(t, l, _) {
+            "use strict";
+            _.r(l);
+            var e = {
+                    components: {
+                        SettingsIcon: _(110).e
+                    },
+                    data: function() {
+                        return {
+                            isLoadingVideo: !0
+                        }
+                    }
+                },
+                n = _(8),
+                component = Object(n.a)(e, (function() {
+                    var t = this,
+                        l = t.$createElement,
+                        _ = t._self._c || l;
+                    return _("div", {
+                        staticClass: "columns"
+                    }, [_("div", {
+                        staticClass: "column is-half-desktop"
+                    }, [_("h2", {
+                        staticClass: "title"
+                    }, [t._v("How to make a deposit")]), t._v(" "), t._m(0), t._v(" "), _("h2", {
+                        staticClass: "title"
+                    }, [t._v("How to make a withdraw with relayer")]), t._v(" "), t._m(1), t._v(" "), _("h2", {
+                        staticClass: "title"
+                    }, [t._v("How to make a withdraw with metamask")]), t._v(" "), _("div", {
+                        staticClass: "content"
+                    }, [_("ul", {
+                        staticClass: "info-list"
+                    }, [_("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Enter your deposit's note")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click settings icon "), _("SettingsIcon")], 1), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Choose Wallet option")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click Save button")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Enter recipient address")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click Withdraw")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Wait until proof is generated")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click Withdraw")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Sign transaction")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Wait until transaction is mined")])]), t._v(" "), _("p", [t._v("Done!")])])])])
+                }), [function() {
+                    var t = this,
+                        l = t.$createElement,
+                        _ = t._self._c || l;
+                    return _("div", {
+                        staticClass: "content"
+                    }, [_("ul", {
+                        staticClass: "info-list"
+                    }, [_("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Choose a token")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Select amount to deposit")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click Deposit")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Backup the note")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click Send Deposit")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Confirm the transaction")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Wait until transaction is mined")])]), t._v(" "), _("p", [t._v("Done!")])])
+                }, function() {
+                    var t = this,
+                        l = t.$createElement,
+                        _ = t._self._c || l;
+                    return _("div", {
+                        staticClass: "content"
+                    }, [_("ul", {
+                        staticClass: "info-list"
+                    }, [_("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Enter your deposit's note")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Enter recipient address")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click Withdraw")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Wait until proof is generated")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Click Withdraw")]), t._v(" "), _("li", {
+                        staticClass: "info-list__item"
+                    }, [t._v("Wait until transaction is mined")])]), t._v(" "), _("p", [t._v("Done!")])])
+                }], !1, null, null, null);
+            l.default = component.exports
+        }
+    }
+]);
